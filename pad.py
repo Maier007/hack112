226dec651ec7ede7f9dc3d02d85e73fe503eb685
melissady <33530708+melissady@users.noreply.github.com>
2017-11-11 21:30:47 -0500

    Update pad.py

--- pad.py
+++ pad.py
@@ -1,7 +1,7 @@
 from noteClass import*
 
 KEY = [WholeNote,HalfNote,QuarterNote,EighthNote,WholeRest,HalfRest,QuarterRest,EighthRest,'.','Sharp','Flat','Natural']
-CELLSIZE = 40
+CELLSIZE = 30
 NUMCELL = len(KEY)
 COLS = 1
 ROWS = NUMCELL//COLS+min(NUMCELL%COLS,1) #Round up
@@ -11,7 +11,7 @@ def drawPad(canvas,data):
     startY = data.py
     endX = data.px+COLS*CELLSIZE
     endY = data.py+ROWS*CELLSIZE
-    canvas.create_rectangle(startX,startY,endX,endY,fill='cyan')
+    canvas.create_rectangle(startX,startY,endX,endY,fill='pale turquoise')
     for row in range(1,ROWS):
         canvas.create_line(startX,startY+row*CELLSIZE,endX,startY+row*CELLSIZE)
     for col in range(1,COLS):
@@ -28,7 +28,7 @@ def drawPad(canvas,data):
             startY = data.py+row*CELLSIZE
             endX = data.px+(col+1)*CELLSIZE
             endY = data.py+(row+1)*CELLSIZE
-            canvas.create_rectangle(startX,startY,endX,endY,fill='pink')
+            canvas.create_rectangle(startX,startY,endX,endY,fill='gold')
 
 
 def clickPad(event,data):
@@ -51,4 +51,3 @@ def clickPad(event,data):
         data.nextNote = KEY[index]
         return True
     return False
-        
